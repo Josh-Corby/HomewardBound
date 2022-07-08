79ca7694b2f94ffcc68d31f221780662f4e51440
Antic-Mii <85930811+Antic-Mii@users.noreply.github.com>
2022-07-09 00:11:49 +1000

    Test

--- Assets/Inventory/Item.cs
+++ Assets/Inventory/Item.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Item/Create New Item")]
 public class Item : ScriptableObject
