3e781255bc3c902dedaf954fee0de59d26a4724a
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2022-07-14 02:18:25 +1000

    small changes

--- Assets/Scripts/HawkDetection.cs
+++ Assets/Scripts/HawkDetection.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HawkDetection : MonoBehaviour
+public class HawkDetection : GameBehaviour
 {
     public GameObject detectionCircle;
     public bool isGrowing;
@@ -10,21 +10,33 @@ public class HawkDetection : MonoBehaviour
     public Vector3 maxScale;
     float lerpTime = 0.3f;
 
+    public float timer;
+    float maxTimer = 3f;
+
 
     private void Start()
     {
         detectionCircle.transform.localPosition = new Vector3(0, 0, 0);
         isGrowing = false;
+        timer = maxTimer;
     }
     private void Update()
     {
+        timer = Mathf.Clamp(timer, 0, maxTimer);
         if (isGrowing)
         {
             detectionCircle.transform.localScale = Vector3.Lerp(detectionCircle.transform.localScale, maxScale,
                 lerpTime * Time.deltaTime);
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                GM.RespawnPlayer();
+                timer = maxTimer;
+            }
         }
         if (!isGrowing)
         {
+            timer += Time.deltaTime;
             detectionCircle.transform.localScale = Vector3.Lerp(detectionCircle.transform.localScale, minScale,
                 lerpTime * Time.deltaTime);
         }
