ca6a3b9b397aa8ec05e9cf5266c97cdf663bb55e
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2022-07-20 18:14:45 +1000

    cat ai

---
+++ Assets/Scripts/CatManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum AIStates
+{
+    Walk,
+    Detecting,
+    Aggro
+}
+public class CatManager : MonoBehaviour
+{
+    public CatDetection catDetection;
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    public AIStates aiState;
+    public GameObject player;
+    private bool resuming;
+
+    private readonly float moveSpeed = 1f;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        aiState = AIStates.Walk;
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void ResumePath()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint) % points.Length;
+    }
+
+    void Update()
+    {
+        switch (aiState)
+        {
+            case AIStates.Walk:
+                {
+                    agent.speed = 3.5f;
+                    if (resuming)
+                    {
+                        ResumePath();
+                        resuming = false;
+                        return;
+                    }
+                    if (!resuming)
+                    {
+                        // Choose the next destination point when the agent gets
+                        // close to the current one.
+                        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                            GotoNextPoint();
+                    }
+                   
+                    break;
+                }
+            case AIStates.Detecting:
+                {
+                    agent.SetDestination(transform.position);
+                    //agent.ResetPath();
+                    resuming = true;
+                    break;
+                }
+            case AIStates.Aggro:
+                {
+                    agent.SetDestination(player.transform.position);
+                    agent.speed = 15;
+                    //transform.position =  Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+                    break;
+                }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            catDetection.raycasting = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            catDetection.raycasting = false;
+            aiState = AIStates.Walk;
+        }
+    }
+}
+
+
+
+
+
+  
+
