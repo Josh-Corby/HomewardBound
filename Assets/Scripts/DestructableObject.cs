021daa8beb6fb81d2335129eb85a73976271f6a1
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2022-08-19 20:00:47 +1000

    grass shader imported, grapple object outline pulse

---
+++ Assets/Scripts/DestructableObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableObject : GameBehaviour
+{
+    public GameObject childObject = null;
+    private Rigidbody childRB;
+
+    private void Start()
+    {
+        if(childRB != null)
+            childRB = childObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            if(childObject != null)
+            {
+                childRB.useGravity = true;
+                childRB.constraints = ~RigidbodyConstraints.FreezePosition;
+            }
+        
+            Destroy(this.gameObject);
+        }
+    }
+}
