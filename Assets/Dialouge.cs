bcb91e4c43986fa0a4482611c0016a37f9197a0f
Antic-Mii <85930811+Antic-Mii@users.noreply.github.com>
2022-09-30 13:50:24 +1000

    Dialogue To NPC working
    working a little bit

--- Assets/Dialouge.cs
+++ Assets/Dialouge.cs
@@ -1,64 +1,157 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class Dialouge : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string[] lines;
-    public float textSpeed;
-
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
     private int index;
 
-    void Start()
-    {
-        textComponent.text = string.Empty;
-        StartDialouge();
-    }
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool playerIsClose;
 
-    // Update is called once per frame
+    //public KeyCode key;
+    //private Button button;
+
+    //private void Awake()
+    //{
+    //    button = GetComponent<Button>();
+    //}
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //if (Input.GetKeyDown(key))
+        //{
+        //    button.onClick.Invoke();
+        //}
+
+        if(Input.GetKeyDown(KeyCode.E) && playerIsClose)
         {
-            if(textComponent.text == lines[index])
+            if (dialoguePanel.activeInHierarchy)
             {
-                NextLine();
+                zeroText();
             }
             else
             {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
             }
         }
-    }
 
-    void StartDialouge()
-    {
-        index = 0;
-        StartCoroutine(TypeLine());
-    }
-
-    IEnumerator TypeLine()
-    {
-        foreach (char c in lines[index].ToCharArray())
+        if (dialogueText.text == dialogue[index])
         {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
+            contButton.SetActive(true);
         }
     }
 
-    void NextLine()
+    public void zeroText()
     {
-        if (index < lines.Length - 1)
+        dialogueText.text = " ";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+
+        if(index < dialogue.Length - 1)
         {
             index++;
-            textComponent.text = string.Empty;
-            StartCoroutine(TypeLine());
+            dialogueText.text = " ";
+            StartCoroutine(Typing());
         }
         else
         {
-            gameObject.SetActive(false);
+            zeroText();
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
+        }
+    }
+
+    //public TextMeshProUGUI textComponent;
+    //public string[] lines;
+    //public float textSpeed;
+
+    //private int index;
+
+    //void Start()
+    //{
+    //    textComponent.text = string.Empty;
+    //    StartDialouge();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.E))
+    //    {
+    //        if (textComponent.text == lines[index])
+    //        {
+    //            NextLine();
+    //        }
+    //        else
+    //        {
+    //            StopAllCoroutines();
+    //            textComponent.text = lines[index];
+    //        }
+    //    }
+    //}
+
+    //void StartDialouge()
+    //{
+    //    index = 0;
+    //    StartCoroutine(TypeLine());
+    //}
+
+    //IEnumerator TypeLine()
+    //{
+    //    foreach (char c in lines[index].ToCharArray())
+    //    {
+    //        textComponent.text += c;
+    //        yield return new WaitForSeconds(textSpeed);
+    //    }
+    //}
+
+    //void NextLine()
+    //{
+    //    if (index < lines.Length - 1)
+    //    {
+    //        index++;
+    //        textComponent.text = string.Empty;
+    //        StartCoroutine(TypeLine());
+    //    }
+    //    else
+    //    {
+    //        gameObject.SetActive(false);
+    //    }
+    //}
 }
