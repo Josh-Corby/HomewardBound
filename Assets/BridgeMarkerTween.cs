a702a951fcffa7e4987d7086b8c893dd208c5f9c
Josh-Corby <85930702+Josh-Corby@users.noreply.github.com>
2022-12-08 12:02:02 +1100

    bridgemarker

--- Assets/BridgeMarkerTween.cs
+++ Assets/BridgeMarkerTween.cs
@@ -10,8 +10,44 @@ public class BridgeMarkerTween : MonoBehaviour
     [SerializeField]
     private float _tweenTime;
 
+    [SerializeField]
+    private Color canBuildColour;
+
+    [SerializeField]
+    private Color cantBuildColour;
+
+    private MeshRenderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        BuildManager.OnCanBuildStatus += BuildStatusColour;
+    }
+
+    private void OnDisable()
+    {
+        BuildManager.OnCanBuildStatus -= BuildStatusColour;
+    }
     private void Start()
     {
         transform.DOScale(_tweenScale, _tweenTime).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+    }
+    private void BuildStatusColour(bool value)
+    {
+        if(value == true)
+        {
+            renderer.material.color = canBuildColour;
+            renderer.material.SetColor("Emission", canBuildColour);
+        }
+
+        if (value == false)
+        {
+            renderer.material.color = cantBuildColour;
+            renderer.material.SetColor("Emission", cantBuildColour);
+        }
     }
 }
